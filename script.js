760ea33849eea3af517027e5dff1a1b83e51c4d1
yaya-mamoudou <yayamamoudou0@gmail.com>
2022-03-21 19:04:52 +0100

    Bug fix

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const menu = document.getElementsByClassName('links');
 const iconMenu = document.getElementsByClassName('menu_toggle_icon');
 const sideMenu = document.getElementsByClassName('side_menu');
-
+let height;
 const openMenu = () => {
   if (menu[0].clientHeight > 0) {
+    height = menu[0].clientHeight;
     iconMenu[0].innerHTML = '<i class="fas fa-caret-down menu_toggle_icon"></i>';
-    menu[0].style.cssText = 'height:0px;overflow:hidden;';
+    menu[0].style.cssText = 'height:0px;overflow:hidden;transition:0.4s;';
   } else {
     iconMenu[0].innerHTML = '<i class="fas fa-caret-up menu_toggle_icon"></i>';
-    menu[0].style.cssText = 'height:fit-content';
+    menu[0].style.cssText = `height:250px; transition:0.4s;`;
   }
 };
 
